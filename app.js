bc29bad465a6fff147e2aff53320ae67c6e2fcc0
AnandDev-06 <anand.06.dev@gmail.com>
2025-07-09 22:52:02 +0530

    added / route to app.js

--- app.js
+++ app.js
@@ -94,6 +94,10 @@ app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 app.use('/info', staticRoutes);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page Not Found !"));
 });
